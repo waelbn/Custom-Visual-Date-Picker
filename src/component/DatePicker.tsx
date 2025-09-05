62aeb392ba2c0d04a4df0adc5fb0f42ad5f054f9
waeltoken <wsboxtunisia@gmail.com>
2025-09-05 18:23:17 +0100

    feat: create initial DatePicker component
    - Create initial React DatePicker component
    - Test and debug initial component
    - Fix TypeScript configuration

---
+++ src/component/DatePicker.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export class DatePicker extends React.Component<{}>{
+    render(){
+        return (
+            <div className="text-red-700 text-xl justify-center">
+                Hello, React!
+            </div>
+        )
+    }
+}
+
+export default DatePicker;
